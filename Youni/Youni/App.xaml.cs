7d8ac0442b649d5725ab35e2d210b5abd7315716
Maxinho96 <maxbrux@live.it>
2017-11-05 12:11:30 +0100

    Abbozzo di Login

--- Youni/Youni/App.xaml.cs
+++ Youni/Youni/App.xaml.cs
@@ -1,14 +1,41 @@
 ﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Youni.Helpers;
 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Youni
 {
     public partial class App : Application
     {
+
+        public string IsFirstTime
+        {
+            get { return Settings.GeneralSettings; }
+            set
+            {
+                if (Settings.GeneralSettings == value)
+                    return;
+                Settings.GeneralSettings = value;
+                OnPropertyChanged();
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new YouniPage();
+            // L'app è stata aperta per la prima volta?
+            if (IsFirstTime == "yes")
+            {
+                // E' la prima volta
+                IsFirstTime = "no";
+                MainPage = new PresentationPage();
+            }
+            else
+            {
+                // Non è la prima volta
+                MainPage = new LoginPage();
+            }
         }
 
         protected override void OnStart()
