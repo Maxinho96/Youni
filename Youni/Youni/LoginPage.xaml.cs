acdeed15baead16e632980beb617633b318cc1a6
Maxinho96 <maxbrux@live.it>
2017-11-20 17:46:20 +0100

    -Modifica ai bottoni Android che erano maiuscoli di default -Aggiunta bottone "Registrati" con effetto fade out su Android (dove non è nativo)

--- Youni/Youni/LoginPage.xaml.cs
+++ Youni/Youni/LoginPage.xaml.cs
@@ -11,5 +11,26 @@ namespace Youni
         {
             InitializeComponent();
         }
+
+        void Handle_Pressed(object sender, System.EventArgs e)
+        {
+            #if __ANDROID__
+            registerButton.TextColor = Color.Gray;
+            registerButton.Opacity = 0.1;
+            #endif
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        async void Handle_Released(object sender, System.EventArgs e)
+        {
+            #if __ANDROID__
+            registerButton.TextColor = Color.FromHex("#45BFEE");
+            await registerButton.FadeTo(1, 500);
+            #endif
+        }
     }
 }
