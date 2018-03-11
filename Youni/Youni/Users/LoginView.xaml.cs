ad69ac6f5d0d41e428ead1b8c8d32bcb95b1b33e
Maxinho96 <maxbrux@live.it>
2018-03-11 10:47:59 +0100

    Ora solo su iOS c'è una ScrollView nella LoginView e nella RegistrationView, che serve per non far sovrapporre la tastiera con la form.

--- Youni/Youni/Users/LoginView.xaml.cs
+++ Youni/Youni/Users/LoginView.xaml.cs
@@ -10,33 +10,25 @@ namespace Youni
         {
             InitializeComponent();
 
-            this.LogEmailEntry.ReturnCommand = new Command(() => LogPasswordEntry.Focus());
+#if __IOS__
+            this.LogEmailEntryiOS.ReturnCommand = new Command(() => LogPasswordEntryiOS.Focus());
+#elif __ANDROID__
+            this.LogEmailEntryAndroid.ReturnCommand = new Command(() => LogPasswordEntryAndroid.Focus());
+#endif
         }
 
-        //public LoginView(LoginRegistrationViewModel loginRegistrationViewModel) : this()
-        //{
-        //    this.BindingContext = loginRegistrationViewModel;
-        //}
-
-        void RegistrationSwitch_Handle_Pressed(object sender, System.EventArgs e)
+        void RegistrationSwitch_Handle_Pressed_Android(object sender, System.EventArgs e)
         {
-#if __ANDROID__
             var button = (Button)sender;
             button.TextColor = Color.Gray;
             button.Opacity = 0.1;
-#endif
         }
 
-#if __ANDROID__
-        async
-#endif
-        void RegistrationSwitch_Handle_Released(object sender, System.EventArgs e)
+        async void RegistrationSwitch_Handle_Released_Android(object sender, System.EventArgs e)
         {
-#if __ANDROID__
             var button = (Button)sender;
             button.TextColor = Color.FromHex("#45BFEE");
             await button.FadeTo(1, 500);
-#endif
         }
     }
 }
