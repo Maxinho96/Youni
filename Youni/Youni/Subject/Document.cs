75e9bdcc707b5cca844367db086a953feaa4aba0
MARCO BERBEGLIA <mar.berbeglia@stud.uniroma3.it>
2018-04-14 12:53:45 +0100

    update pagina Corso; inserimento classe Document per gestire i docs presi da amazon S3

---
+++ Youni/Youni/Subject/Document.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Youni
+{
+    public class Document : BindableObject
+    {
+        public string DocumentTitle { get; set; }
+        public Image DocumentImage { get; set; }
+        public int DocumentUpvote { get; set; }
+
+        public Document()
+        {
+        }
+
+        public Document(string documentTitle, Image documentImage, int documentUpvote) : this()
+        {
+            this.DocumentTitle = documentTitle;
+            this.DocumentImage = documentImage;
+            this.DocumentUpvote = documentUpvote;
+        }
+    }
+}
--- Youni/Youni/Subject/SubjectPageViewModel.cs
+++ Youni/Youni/Subject/SubjectPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -11,7 +12,21 @@ namespace Youni
         public INavigation Navigation;
         public Command NotifyTapped { get; set; }
         public Command FavouritesTapped { get; set; }
+        public Command SearchCommand { get; set; }
         public string SubjectName { get; set; }
+        private ObservableCollection<Document> documentsList;
+        public ObservableCollection<Document> DocumentsList
+        {
+            get
+            {
+                return this.documentsList;
+            }
+            set
+            {
+                this.documentsList = value;
+                OnPropertyChanged("DocumentsList");
+            }
+        }
 
         public SubjectPageViewModel(string subjectName)
         {
@@ -24,9 +39,14 @@ namespace Youni
             {
 
             });
+            this.SearchCommand = new Command(async () =>
+            {
+                await Application.Current.MainPage.DisplayAlert("titolo", "RICERCA", "cancel");
+            });
         }
         public async Task GetResources()
         {
+
         }
 
 
